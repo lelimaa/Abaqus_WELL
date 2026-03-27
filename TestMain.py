ee379d766e5808660cf59bfb093a8a92d0e8ab97
lelimaa <le.oliveiira@gmail.com>
2026-03-27 11:38:31 -0300

    changes to leticia

--- TestMain.py
+++ TestMain.py
@@ -3,19 +3,20 @@ from abaqusConstants import *
 
 # import os
 import json
-import sys 
+import sys
 
-# path_project = r'C:\Users\juani\Documents\Github\Abaqus_WELL_' 
-path_project = r'C:\Users\hidalgo\Documents\GitHub\Abaqus_WELL_'
+# path_project = r'C:\Users\juani\Documents\Github\Abaqus_WELL_'
+# path_project = r'C:\Users\hidalgo\Documents\GitHub\Abaqus_WELL_'
+path_project = r'C:\Users\leticia\Documents\GitHub\Abaqus_WELL_'
 
 if path_project not in sys.path:
     sys.path.append(path_project)
 
-from GEOMETRY.geometries import * 
-from GEOMETRY.assembly import *
-from GEOMETRY.sets import *
+from JSONS.ImportTools import process_lithology
+from GEOMETRY.geometries import *
 from MATERIALS.materials import *
-from JSONS.ImportTools import process_lithology             
+from GEOMETRY.sets import *
+from GEOMETRY.assembly import *
 
 mdb.models.changeKey(fromName='Model-1', toName='MyFirstModel')
 
@@ -25,7 +26,8 @@ if 'MyFirstModel' not in mdb.models:
 # Reading the json file and filling the input data for the analysis ####################
 
 # with open(r'C:\Users\juani\Documents\Github\Abaqus_WELL_\wellClosure_axi.json') as f:
-with open(r'C:\Users\hidalgo\Documents\GitHub\Abaqus_WELL_\wellClosure_axi.json') as f:
+# with open(r'C:\Users\hidalgo\Documents\GitHub\Abaqus_WELL_\wellClosure_axi.json') as f:
+with open(r'C:\Users\leticia\Documents\GitHub\Abaqus_WELL_\wellClosure_axi.json') as f:
     data = json.load(f)
 
 print(f"Data keys: {data.keys()}")
@@ -38,7 +40,7 @@ name_tubular = 'VAM_16in_#97_P110'
 outer_diamenter_pipe = data["Tubulars"][name_tubular]['OD']
 thickness_pipe = data["Tubulars"][name_tubular]['Thickness']
 thickness_pipe = thickness_pipe * 0.0254  # Convert from inches to meters
-inner_radius_pipe = outer_diamenter_pipe / 2 - thickness_pipe 
+inner_radius_pipe = outer_diamenter_pipe / 2 - thickness_pipe
 inner_radius_pipe = inner_radius_pipe * 0.0254  # Convert from inches to meters
 inner_radius_annular = inner_radius_pipe + thickness_pipe
 diameter_wellbore = data["Phases"][name_phase]['HoleDiameter']
@@ -47,9 +49,9 @@ inner_radius_wellbore = diameter_wellbore / 2
 thickness_annular = inner_radius_wellbore - inner_radius_annular
 thickness_wellbore = 12.0   # Verificar o valor máximo com testes
 
-base_depth = data["AnalysisData"]["Bottom"] 
+base_depth = data["AnalysisData"]["Bottom"]
 base_depth = int(base_depth)
-top_depth = data["AnalysisData"]["Top"] 
+top_depth = data["AnalysisData"]["Top"]
 top_depth = int(top_depth)
 
 print(f"The bottom of the wellbore is at: {-base_depth} meters")
@@ -59,7 +61,8 @@ print(f"The top of the wellbore is at: {-top_depth} meters")
 
 if __name__ == "__main__":
 
-    filtered_layers, t_depths, filtered_rocks = process_lithology(data, top_depth, base_depth) 
+    filtered_layers, t_depths, filtered_rocks = process_lithology(
+        data, top_depth, base_depth)
     layers_depths = sorted(t_depths)
 
     data_code = {
@@ -84,8 +87,9 @@ if __name__ == "__main__":
     }
 
 for part_name, part_data in data_code.items():
-        CreateGeometry('MyFirstModel', part_name, part_data)
-        PartitionLayersByDepth("MyFirstModel", part_name=part_name, layer_depths=part_data["layer_depths"])
+    CreateGeometry('MyFirstModel', part_name, part_data)
+    PartitionLayersByDepth("MyFirstModel", part_name=part_name,
+                           layer_depths=part_data["layer_depths"])
 
 # Definition of materials ###############################################################
 
@@ -97,20 +101,21 @@ if __name__ == "__main__":
     end_search = base_depth
 
     filtered_data = [
-        item for item in lithology 
+        item for item in lithology
         if item["Top"] < end_search and item["Bottom"] > init_search
-        ]
+    ]
 
     rock_in_region = {item["Rock"] for item in filtered_data}
 
     examples = {}
 
     casing_type = "P110"
+
     examples["STEEL"] = {
         "behavior": data["SteelGrades"][casing_type]["Law"],
         'density': data["SteelGrades"][casing_type]["ElasticParameters"]["Density"],
         'elastic': (data["SteelGrades"][casing_type]["ElasticParameters"]["Young"]*1e9,
-                     data["SteelGrades"][casing_type]["ElasticParameters"]["Poisson"]),
+                    data["SteelGrades"][casing_type]["ElasticParameters"]["Poisson"]),
         'conductivity': data["SteelGrades"][casing_type]["ThermalParameters"]["Conductivity"],
         'specific_heat': data["SteelGrades"][casing_type]["ThermalParameters"]["SpecificHeat"],
         'expansion': data["SteelGrades"][casing_type]["ThermalParameters"]["ThermalExpansion"],
@@ -124,7 +129,7 @@ if __name__ == "__main__":
         'conductivity': 0.702,
         'specific_heat': 2060.0,
         "type": "Fluid"
-    }    
+    }
 
     for mat_name, properties in filtered_rocks.items():
 
@@ -146,12 +151,11 @@ if __name__ == "__main__":
                 'dilatancy_angle': mc["DilatancyAngle"],
                 'cohesion': mc["Cohesion"],
                 "lab_data": ((20001698.76, 0.0), )
-                })
+            })
             # examples[rock_name]['lab_data'] = ((10e6, 0.0), (20e6, 0.01), (30e6, 0.03), (40e6, 0.06))
 
         if "DoublePowerParameters" in properties:
             examples[mat_name]["DoublePowerParameters"] = properties["DoublePowerParameters"]
-
 
     material_examples = {
         "PIPE": {
@@ -166,12 +170,10 @@ if __name__ == "__main__":
         }
     }
 
-
     lythology_examples = []
 
-    import pprint 
+    import pprint
     pprint.pprint(filtered_layers)
-
 
     for i, layer in enumerate(filtered_layers, start=1):
 
@@ -186,7 +188,6 @@ if __name__ == "__main__":
 
         lythology_examples.append(new_block)
 
-
     for mat_name, mat_data in examples.items():
         CreateMaterial('MyFirstModel', mat_name, mat_data, sectionLength=1.)
 
@@ -194,21 +195,20 @@ if __name__ == "__main__":
     CreateSetsFluid('MyFirstModel')
     CreateSetsRock('MyFirstModel')
 
-
     for section_name in material_examples.values():
         Assign_Section('MyFirstModel',
-                        partName=section_name["partName"],
-                        sectionName=section_name["sectionName"],
-                        isSolid=section_name["isSolid"])
- 
+                       partName=section_name["partName"],
+                       sectionName=section_name["sectionName"],
+                       isSolid=section_name["isSolid"])
+
     # Assign rock materials by depth layers
     AssignRockByDepth('MyFirstModel', 'ROCK', lythology_examples)
 
     # Create assembly
     Assembly('MyFirstModel', partsNames=['FLUID', 'PIPE', 'ROCK'],
-                top_depth=top_depth, base_depth=base_depth)  
-        
+             top_depth=top_depth, base_depth=base_depth)
+
     # Defining sets for boundary conditions and interactions
-    CreateSetsAssembly('MyFirstModel')   
+    CreateSetsAssembly('MyFirstModel')
 
     CreateSurfacesAssembly('MyFirstModel', data_code)
