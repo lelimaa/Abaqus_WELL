6e96b814c2fc0ba6158428e8fcdefaea58c1f4fc
lelimaa <le.oliveiira@gmail.com>
2026-05-27 14:24:28 -0300

    nome da pasta alterado

--- TestMain.py
+++ TestMain.py
@@ -6,7 +6,7 @@ import json
 import sys
 
 # path_project = r'C:\Users\juani\Documents\Github\Abaqus_WELL_'
-path_project = r'C:\Users\leticia\Documents\GitHub\Abaqus_WELL_'
+path_project = r'C:\Users\leticia\Documents\GitHub\Abaqus_WELL'
 
 if path_project not in sys.path:
     sys.path.append(path_project)
@@ -31,7 +31,7 @@ if 'MyFirstModel' not in mdb.models:
 # Reading the json file and filling the input data for the analysis ####################
 
 # with open(r'C:\Users\juani\Documents\Github\Abaqus_WELL_\wellClosure_axi.json') as f:
-with open(r'C:\Users\leticia\Documents\GitHub\Abaqus_WELL_\wellClosure_axi.json') as f:
+with open(r'C:\Users\leticia\Documents\GitHub\Abaqus_WELL\wellClosure_axi.json') as f:
     data = json.load(f)
 
 print(f"Data keys: {data.keys()}")
