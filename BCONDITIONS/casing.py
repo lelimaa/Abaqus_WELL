d5ab70e9b59bb8b109bbfbf518086e173cd8c0f8
JuanHidalgo790 <125278160+JuanHidalgo790@users.noreply.github.com>
2026-06-10 15:58:28 -0300

    new casing stress

--- BCONDITIONS/casing.py
+++ BCONDITIONS/casing.py
@@ -2,13 +2,15 @@
 # from abaqusConstants import *
 # import numpy as np
 
-def CasingStresses(data, name_phase, density_steel, z_top, z_bottom):
+def CasingStresses(data, name_phase, density_steel, density_cement, z_top, z_bottom):
 
     density_fluid = 1000  # kg/m³ (density of water)
     gravity = 9.81  # m/s² (acceleration due to gravity)
 
     height_water_above_casing = data["Phases"][name_phase]["Casing"][0]["Top"] 
     length_casing = data["Phases"][name_phase]["Casing"][0]["Bottom"] - data["Phases"][name_phase]["Casing"][0]["Top"]
+    length_cement = data["Phases"][name_phase]["Cement"][0]["Bottom"] - data["Phases"][name_phase]["Cement"][0]["Top"]
+    
     height_base_casing = length_casing + height_water_above_casing
 
     # stress_base = density_fluid * gravity * height_base_casing
